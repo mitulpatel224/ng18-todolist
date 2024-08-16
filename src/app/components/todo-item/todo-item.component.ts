516e365baaf955b60f822fa4406702e736daf10f
mitulpatel224 <mitulpatel224@gmail.com>
2024-08-16 18:09:56 +0530

    fix: show ETA for created task items

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,22 +1,14 @@
-import {
-  Component,
-  EventEmitter,
-  input,
-  model,
-  Output,
-  output,
-  signal,
-} from '@angular/core';
-import { Todo } from '../../models/model';
-import { CommonModule, NgClass } from '@angular/common';
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
-import { SwipeDirective } from '../../directives/swipe.directive';
+import { CommonModule, DatePipe, NgClass } from '@angular/common';
+import { Component, EventEmitter, input, model, Output } from '@angular/core';
+import { HammerModule } from '@angular/platform-browser';
 import { SwipeEvent } from '../../directives/swipe-core';
+import { SwipeDirective } from '../../directives/swipe.directive';
+import { Todo } from '../../models/model';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [NgClass, HammerModule, SwipeDirective, CommonModule],
+  imports: [NgClass, HammerModule, SwipeDirective, CommonModule, DatePipe],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss',
 })
@@ -26,6 +18,8 @@ export class TodoItemComponent {
 
   /** Model Signal: to allow view more detail from parent component */
   showMore = model<boolean>(false);
+
+  etaFormat: 'MMM dd' | 'MMM dd, YYYY' = 'MMM dd';
 
   /** Output: Event to perform action on task status change */
   @Output() markCheck = new EventEmitter<void>(true);
@@ -38,6 +32,7 @@ export class TodoItemComponent {
    * @param flag Boolean
    */
   toggleDescription(flag: boolean) {
+    this.etaFormat = flag ? 'MMM dd, YYYY' : 'MMM dd';
     this.showMore.update(() => flag);
   }
 
